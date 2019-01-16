31f075b5e4d9003116969877aa2b54a018a5d144
Toni Huovinen <toni.huovinen@edu.turkuamk.fi>
2019-01-16 19:45:18 +0200

    Created first draft of the exercise screen as discussed in class. Wired it to work in week 1 for now, also you can press the image on warmup1 to get alert dialog. This could be a possible location of the gif.

--- app/src/main/java/com/sabateam/saba/ProgramScreen.java
+++ app/src/main/java/com/sabateam/saba/ProgramScreen.java
@@ -1,13 +1,33 @@
 package com.sabateam.saba;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ProgramScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_screen);
+
+
+    }
+
+    // Temp method for button
+    public void TempShowAlert(View view){
+
+        ImageView image = new ImageView(this);
+        image.setImageResource(R.drawable.nwuni_logo);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setMessage("Gif of the move would be here, maybe")
+                .setPositiveButton("Done", null).setView(image);
+
+        builder.create().show();
     }
 }
--- app/src/main/java/com/sabateam/saba/ProgramSelectionScreen.java
+++ app/src/main/java/com/sabateam/saba/ProgramSelectionScreen.java
@@ -1,7 +1,9 @@
 package com.sabateam.saba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProgramSelectionScreen extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ProgramSelectionScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_selection_screen);
+    }
+
+    public void TempButtonClick(View view) {
+
+        Intent intent = new Intent(this, ProgramScreen.class);
+        startActivity(intent);
     }
 }
