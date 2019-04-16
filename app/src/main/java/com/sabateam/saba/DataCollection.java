b34964b73ec961c87132b5a3099c0fa559ec340f
Joel Juntunen <joel.juntunen@edu.turkuamk.fi>
2019-04-16 13:43:07 +0300

    changed FAQ DB structure

--- app/src/main/java/com/sabateam/saba/DataCollection.java
+++ app/src/main/java/com/sabateam/saba/DataCollection.java
@@ -39,86 +39,35 @@ public class DataCollection implements Serializable {
     private String userLog;
     private String avatar;
     private Integer FAQAccessed;
-    private Integer fdata1;
-    private Integer fdata2;
-    private Integer fdata3;
-    private Integer fdata4;
-    private Integer fdata5;
-    private Integer fdata6;
-    private Integer fdata7;
-    private Integer fdata8;
-    private Integer fdata9;
-    private Integer fdata10;
-    private Integer fdata11;
-    private Integer fdata12;
-    private Integer fdata13;
-    private Integer fdata14;
-    private Integer fdata15;
-    private Integer fdata16;
-    private Integer fdata17;
-    private Integer fdata18;
     private Integer sentToPhone;
     private Integer trainingProgrammesAccessed;
     private String exerciseViewed;
     private String dataBaseWeekAccessed;
     private String dbHowToNew;
+    private String faqAccessNew;
 
 
 
     public DataCollection(String userLog,
                           String avatar,
                           Integer FAQAccessed,
-                          Integer fdata1,
-                          Integer fdata2,
-                          Integer fdata3,
-                          Integer fdata4,
-                          Integer fdata5,
-                          Integer fdata6,
-                          Integer fdata7,
-                          Integer fdata8,
-                          Integer fdata9,
-                          Integer fdata10,
-                          Integer fdata11,
-                          Integer fdata12,
-                          Integer fdata13,
-                          Integer fdata14,
-                          Integer fdata15,
-                          Integer fdata16,
-                          Integer fdata17,
-                          Integer fdata18,
                           Integer sentToPhone,
                           Integer trainingProgrammesAccessed,
                           String exerciseViewed,
                           String dataBaseWeekAccessed,
-                          String dbHowToNew
+                          String dbHowToNew,
+                          String faqAccessNew
 
     ) {
         this.userLog = userLog;
         this.avatar = avatar;
         this.FAQAccessed = FAQAccessed;
-        this.fdata1 = fdata1;
-        this.fdata2 = fdata2;
-        this.fdata3 = fdata3;
-        this.fdata4 = fdata4;
-        this.fdata5 = fdata5;
-        this.fdata6 = fdata6;
-        this.fdata7 = fdata7;
-        this.fdata8 = fdata8;
-        this.fdata9 = fdata9;
-        this.fdata10 = fdata10;
-        this.fdata11 = fdata11;
-        this.fdata12 = fdata12;
-        this.fdata13 = fdata13;
-        this.fdata14 = fdata14;
-        this.fdata15 = fdata15;
-        this.fdata16 = fdata16;
-        this.fdata17 = fdata17;
-        this.fdata18 = fdata18;
         this.sentToPhone = sentToPhone;
         this.trainingProgrammesAccessed = trainingProgrammesAccessed;
         this.exerciseViewed = exerciseViewed;
         this.dataBaseWeekAccessed = dataBaseWeekAccessed;
         this.dbHowToNew  = dbHowToNew;
+        this.faqAccessNew = faqAccessNew;
     }
 
     public DataCollection() {
@@ -135,71 +84,15 @@ public class DataCollection implements Serializable {
 
     public int getFAQAccessed() { return FAQAccessed;}
 
-    public int getfData1() {
-        return fdata1;
-    }
-    public int getfData2() {
-        return fdata2;
-    }
-    public int getfData3() {
-        return fdata3;
-    }
-    public int getfData4() {
-        return fdata4;
-    }
-    public int getfData5() {
-        return fdata5;
-    }
-    public int getfData6() {
-        return fdata6;
-    }
-    public int getfData7() {
-        return fdata7;
-    }
-    public int getfData8() {
-        return fdata8;
-    }
-    public int getfData9() {
-        return fdata9;
-    }
-    public int getfData10() {
-        return fdata10;
-    }
-    public int getfData11() {
-        return fdata11;
-    }
-    public int getfData12() {
-        return fdata12;
-    }
-    public int getfData13() {
-        return fdata13;
-    }
-    public int getfData14() {
-        return fdata14;
-    }
-    public int getfData15() {
-        return fdata15;
-    }
-    public int getfData16() {
-        return fdata16;
-    }
-    public int getfData17() {
-        return fdata17;
-    }
-    public int getfData18() {
-        return fdata18;
-    }
-
     public int getsentToPhone() {return sentToPhone;}
 
     public int gettrainingProgrammesAccessed() {return trainingProgrammesAccessed;}
 
-    public String getexerciseViewed() {
-        return exerciseViewed;
-    }
+    public String getexerciseViewed() {return exerciseViewed;}
 
     public String getdataBaseWeekAccessed() {return  dataBaseWeekAccessed;}
 
     public String getdbHowToNew() {return dbHowToNew; }
+    public String getfaqAccessNew() {return  faqAccessNew;}
 
 }
--- app/src/main/java/com/sabateam/saba/MenuScreen.java
+++ app/src/main/java/com/sabateam/saba/MenuScreen.java
@@ -103,8 +103,7 @@ public class MenuScreen extends AppCompatActivity {
     private void Logout(){
         SendToDatabase();
         flushSharedPreferences();
-        flushDBHTBA();
-        flushFAQDB();
+        flushStatics();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
@@ -116,31 +115,9 @@ public class MenuScreen extends AppCompatActivity {
         editor.commit();
     }
 
-    public void flushDBHTBA(){
+    public void flushStatics(){
         BeActiveScreen.dbHowTo = "";
-    }
-
-    public void flushFAQDB(){
-        FAQScreen.faq1Access = 0;
-        FAQScreen.faq2Access = 0;
-        FAQScreen.faq3Access = 0;
-        FAQScreen.faq4Access = 0;
-        FAQScreen.faq5Access = 0;
-        FAQScreen.faq6Access = 0;
-        FAQScreen.faq7Access = 0;
-        FAQScreen.faq8Access = 0;
-        FAQScreen.faq9Access = 0;
-        FAQScreen.faq10Access = 0;
-        FAQScreen.faq11Access = 0;
-        FAQScreen.faq12Access = 0;
-        FAQScreen.faq13Access = 0;
-        FAQScreen.faq14Access = 0;
-        FAQScreen.faq15Access = 0;
-        FAQScreen.faq16Access = 0;
-        FAQScreen.faq17Access = 0;
-        FAQScreen.faq18Access = 0;
-        FAQScreen.faq19Access = 0;
-
+        FAQScreen.faqAccess = "";
     }
 
 
@@ -150,58 +127,24 @@ public class MenuScreen extends AppCompatActivity {
         String userLog = "user:" + DataCollection.getStringForDataBase(this, "userName");
         String avatar = DataCollection.getStringForDataBase(this, "selectedAvatar");
         Integer FAQAccessed = DataCollection.getIntForDataBase(this, "faqPageAccessed");
-        Integer faqdata1 = DataCollection.getIntForDataBase(this, "faq1Access");
-        Integer faqdata2 = DataCollection.getIntForDataBase(this, "faq2Access");
-        Integer faqdata3 = DataCollection.getIntForDataBase(this, "faq3Access");
-        Integer faqdata4 = DataCollection.getIntForDataBase(this, "faq4Access");
-        Integer faqdata5 = DataCollection.getIntForDataBase(this, "faq5Access");
-        Integer faqdata6 = DataCollection.getIntForDataBase(this, "faq6Access");
-        Integer faqdata7 = DataCollection.getIntForDataBase(this, "faq7Access");
-        Integer faqdata8 = DataCollection.getIntForDataBase(this, "faq8Access");
-        Integer faqdata9 = DataCollection.getIntForDataBase(this, "faq9Access");
-        Integer faqdata10 = DataCollection.getIntForDataBase(this, "faq10Access");
-        Integer faqdata11 = DataCollection.getIntForDataBase(this, "faq11Access");
-        Integer faqdata12 = DataCollection.getIntForDataBase(this, "faq12Access");
-        Integer faqdata13 = DataCollection.getIntForDataBase(this, "faq13Access");
-        Integer faqdata14 = DataCollection.getIntForDataBase(this, "faq14Access");
-        Integer faqdata15 = DataCollection.getIntForDataBase(this, "faq15Access");
-        Integer faqdata16 = DataCollection.getIntForDataBase(this, "faq16Access");
-        Integer faqdata17 = DataCollection.getIntForDataBase(this, "faq17Access");
-        Integer faqdata18 = DataCollection.getIntForDataBase(this, "faq18Access");
         Integer sentToPhone = DataCollection.getIntForDataBase(this, "sentToPhone");
         Integer trainingProgrammesAccessed = DataCollection.getIntForDataBase(this, "trainingProgrammesAccessed");
         String exerciseViewed = DataCollection.getStringForDataBase(this, "exerciseViewed");
         String dataBaseWeekAccessed = DataCollection.getStringForDataBase(this, "dataBaseWeekAccessed");
         String dbHowToNew = DataCollection.getStringForDataBase(this, "dbHowToNew");
+        String faqAccessNew = DataCollection.getStringForDataBase(this, "faqAccessNew");
 
         // This class handles the creation of object that is sent to Firebase
         DataCollection dCollection = new DataCollection(
                 userLog,
                 avatar,
                 FAQAccessed,
-                faqdata1,
-                faqdata2,
-                faqdata3,
-                faqdata4,
-                faqdata5,
-                faqdata6,
-                faqdata7,
-                faqdata8,
-                faqdata9,
-                faqdata10,
-                faqdata11,
-                faqdata12,
-                faqdata13,
-                faqdata14,
-                faqdata15,
-                faqdata16,
-                faqdata17,
-                faqdata18,
                 sentToPhone,
                 trainingProgrammesAccessed,
                 exerciseViewed,
                 dataBaseWeekAccessed,
-                dbHowToNew
+                dbHowToNew,
+                faqAccessNew
         );
 
         // Sends the data to a collection named 'messages' in Firebase
